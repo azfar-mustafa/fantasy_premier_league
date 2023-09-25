3bc67b24ba29f52e5b20501328d74e93d41c962a
Azfar Mustafa <khairulbakar95@gmail.com>
2023-09-25 17:02:22 +0800

    Change folder path

--- silver/3_transform_fixture_metadata.py
+++ silver/3_transform_fixture_metadata.py
@@ -64,7 +64,7 @@ def delete_silver_folder(folder_path):
 if __name__ == "__main__":
     all_dict = []
     current_date = convert_timestamp_to_myt()
-    bronze_file_directory = f"C:/Users/khair/project/fantasy_premier_league/data/bronze/fantasy_premier_league/current_season_history_data/{current_date}/"
+    bronze_file_directory = f"C:/Users/khair/project/fantasy_premier_league/data/bronze/fantasy_premier_league/player_data/{current_date}/"
     silver_file_directory = f"C:/Users/khair/project/fantasy_premier_league/data/silver/fantasy_premier_league/current_season_history_data/{current_date}/"
     silver_json_file = f"current_season_history_fixture_{current_date}.json"
     silver_parquet_file = f"current_season_history_fixture_{current_date}.parquet"
