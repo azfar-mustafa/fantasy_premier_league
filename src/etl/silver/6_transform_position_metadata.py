0b2ead34a45e27558e0df28008f2792dbf58c6d8
azfar mustafa <khairulbakar95@gmail.com>
2023-11-09 22:32:25 +0800

    Add filename into message and change file log name

--- src/etl/silver/6_transform_position_metadata.py
+++ src/etl/silver/6_transform_position_metadata.py
@@ -11,10 +11,10 @@ import time
 
 
 def configure_logging():
-    log_format = "%(asctime)s - %(levelname)s - %(message)s"
+    log_format = "%(asctime)s - %(levelname)s - %(filename)s - %(message)s"
     log_file_current_timestamp = time.strftime("%Y%m%d")
     log_folder_path = "C:/Users/khair/project/fantasy_premier_league/log/"    
-    log_file_name = f"6_transform_position_metadata_{log_file_current_timestamp}.log"
+    log_file_name = f"fantasy_premier_league_{log_file_current_timestamp}.log"
     log_file_path = f"{log_folder_path}{log_file_name}"
     logging.basicConfig(filename=log_file_path, encoding='utf-8', level=logging.INFO, format=log_format)
 
